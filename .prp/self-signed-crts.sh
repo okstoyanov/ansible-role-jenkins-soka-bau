a018459d593b7f7d4b2f2a68dfbccb1b37f90aa8
kstoiano <kostiantyn.stoianov@sva.de>
2024-05-02 13:30:34 +0200

    template nginx conf | install certificates | use 443 | redirect 80 to 443

--- .prp/self-signed-crts.sh
+++ .prp/self-signed-crts.sh
@@ -1,12 +1,6 @@
 ## Info:
 openssl genrsa -out ca.key 4096
 openssl req -new -x509 -days 3650 -key ca.key -out ca.crt
-openssl req -newkey rsa:4096 -nodes -keyout jenkins.key -out jenkins.csr
-openssl x509 -req -extfile <(printf "subjectAltName=DNS:jenkins,DNS:sl-022427.sol.labwi.sva.de") -days 3650 \
--in jenkins.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out jenkins.crt
-openssl pkcs12 -export -out jenkins.p12 \
--passout 'pass:password' -inkey jenkins.key \
--in jenkins.crt -certfile ca.crt -name sl-022427.sol.labwi.sva.de
-
-
-
+openssl req -newkey rsa:4096 -nodes -keyout jenkins_02.key -out jenkins_02.csr
+openssl x509 -req -extfile <(printf "subjectAltName=DNS:jenkins,DNS:sl-023113.sol.labwi.sva.de") -days 3650 -in jenkins_02.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out jenkins_02.crt
+openssl pkcs12 -export -out jenkins_02.p12 -passout 'pass:password' -inkey jenkins_02.key -in jenkins_02.crt -certfile ca.crt -name sl-023113.sol.labwi.sva.de
